44fb3a650905b8dbd6a2886563dca779801b8489
akusukabells <49921710+akusukabells@users.noreply.github.com>
2023-10-30 14:52:57 +0700

    Initial commit

---
+++ app/src/main/java/com/wendot/sinarsuryafurniture/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wendot.sinarsuryafurniture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
